7bf33ab321b3d991aa25479a691bbb58f0d0f1b3
Akseli Koskela <playester@gmail.com>
2016-01-27 15:13:25 +0200

    CD homework

---
+++ KT2/CD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT2
+{
+    class CD
+    {
+        //Put here CD name, artist and maybe price
+    }
+}
---
+++ KT2/Songs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT2
+{
+    class Songs
+    {
+        //Put here song name and lenght
+    }
+}
