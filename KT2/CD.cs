b295128d13c5aa3533334e46fe51010ac15aa5d7
Akseli Koskela <playester@gmail.com>
2016-01-27 15:28:19 +0200

    First steps

--- KT2/CD.cs
+++ KT2/CD.cs
@@ -9,5 +9,9 @@ namespace KT2
     class CD
     {
         //Put here CD name, artist and maybe price
+
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public double Price { get; set; }
     }
 }
